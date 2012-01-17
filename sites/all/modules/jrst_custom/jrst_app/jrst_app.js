692e96e2482fca977446d670bd7647064e3a6c2b
Mark Schulte <schultetwin@gmail.com>
2012-01-17 01:37:14 -0500

    Adding throber

--- sites/all/modules/jrst_custom/jrst_app/jrst_app.js
+++ sites/all/modules/jrst_custom/jrst_app/jrst_app.js
@@ -6,8 +6,13 @@
 
 // $Id$
 Drupal.behaviors.jrst_app = function (context) {
+  var myTimer = undefined;
+  var delayTime = 3000;
+
   $('a.jrstposLink', context).click(function () {
     // This function will get exceuted after the ajax request is completed successfully
+    $(this).addClass('display-throbber');
+
     var jrstposInfo = function(data) {
       // The data parameter is a JSON object. The “products” property is the list of products items that was returned from the server response to the ajax request.
       $('#block-views-jrst_app_pos_info-block_1 .view-jrst-app-pos-info').html(data.jrst_app_view);
@@ -19,6 +24,7 @@ Drupal.behaviors.jrst_app = function (context) {
       dataType: 'json', //define the type of data that is going to get back from the server
       data: 'js=' + $(this).attr("title") //Pass a key/value pair
     });
+
     return false;  // return false so the navigation stops here and not continue to the page in the link
-}).addClass('categoryLink-processed');
+}).removeClass('display-throbber');
 }
